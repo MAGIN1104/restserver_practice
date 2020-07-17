40e92b22c4e19a724465ad588461aa7973d59f98
Magin <magin1104@gmail.com>
2020-07-17 12:40:55 -0400

    Variables de entorno DB

--- server/config/config.js
+++ server/config/config.js
@@ -12,18 +12,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 /**
  * BASE DE DATOS
- * 
- * mongodb://localhost:27017/cafe
- * mongodb+srv://magin:magin123@cluster0.10nqc.mongodb.net/test
- * 
  */
 
  let urlDB;
-//  if(process.env.NODE_ENV === 'dev'){
-//      urlDB='mongodb://localhost:27017/cafe'
-//  }else{
-     urlDB='mongodb+srv://magin:magin123@cluster0.10nqc.mongodb.net/cafe'
-//  }
+ if(process.env.NODE_ENV === 'dev'){
+     urlDB='mongodb://localhost:27017/cafe'
+ }else{
+     urlDB='process.env.NODE_ENV'
+ }
 
  process.env.URLDB=urlDB;
  
