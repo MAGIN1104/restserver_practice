81d9f6d5fcc57b297c8e9b310f95360a5857d8d9
Magin <magin1104@gmail.com>
2020-07-17 12:46:28 -0400

    Variables de entorno DB

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
  if(process.env.NODE_ENV === 'dev'){
      urlDB='mongodb://localhost:27017/cafe'
  }else{
-     urlDB='process.env.MONGO_URI'
+     urlDB=process.env.MONGO_URI
  }
 
  process.env.URLDB=urlDB;
